3060e43f91d8c1d56284d2b171505d7a0416592d
Rebka Ayele Asfaw <Rebka.Ayele@bankofabyssinia.com>
2025-09-08 16:33:35 +0300

    slider added

--- Netflix-clone-project/src/App.jsx
+++ Netflix-clone-project/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home/Home'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
 
--- Netflix-clone-project/src/components/Rows/Row/Row.jsx
+++ Netflix-clone-project/src/components/Rows/Row/Row.jsx
@@ -4,9 +4,10 @@ import { useState } from "react";
 import axios from "../../../utils/axios";
 import movieTrailer from "movie-trailer";
 import YouTube from "react-youtube";
+import Slider from "react-slick";
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovie] = useState([]);
-  const [trailerUrl,setTrailerUrl]=useState("");
+  const [trailerUrl, setTrailerUrl] = useState("");
   const baseUrl = "http://image.tmdb.org/t/p/w500";
   console.log(fetchUrl);
   useEffect(() => {
@@ -21,45 +22,62 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
     })();
   }, [fetchUrl]);
 
-  const handleClick=(movie)=>{
+  const handleClick = (movie) => {
     if (trailerUrl) {
       setTrailerUrl("");
-  }else{
-    movieTrailer(movie?.title || movie?.name || movie?.original_name )
-  .then((url=>{
-    console.log(url);
-    const urlParams=new URLSearchParams(new URL(url).search);
-    console.log(urlParams);
-    console.log(urlParams.get('v'));
-    setTrailerUrl(urlParams.get('v'));
-  }))
-}}
-
-const opts={
-    width:'100%',
-  height:'390',
-  playerVars:{
-    autoplay:1,
-  }
-}
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name).then(
+        (url) => {
+          console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log(urlParams);
+          console.log(urlParams.get("v"));
+          setTrailerUrl(urlParams.get("v"));
+        }
+      );
+    }
+  };
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 10, // Number of cards visible
+    slidesToScroll: 1,
+  
+    responsive: [
+      {
+        breakpoint: 690,
+        settings: { slidesToShow: 1 }
+      }
+    ]
+  };
+  const opts = {
+    width: "100%",
+    height: "390",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
     <div className="row">
       <h1>{title}</h1>
-      <div className="row_posters">
+      <Slider {...settings} className="row_posters slider-container">
         {movies?.map((movie, index) => (
+           <div key={index}>
           <img
-          onClick={()=>handleClick(movie)}
-            key={index}
+            onClick={() => handleClick(movie)}
+           
             src={`${baseUrl}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
             className={`row_poster ${isLargeRow ? "row_posterLarge" : ""}`}
           />
+          </div>
         ))}
-      </div>
-      <div style={{padding:"40px"}}>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
+      </Slider>
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
       </div>
     </div>
   );
